f76c7fbdc12eebdd6569e8d78ed122e55d32c686
kimsolee <ahhasolee@naver.com>
2022-01-07 22:29:28 +0900

    함수 추가
    TODO 작성 함수 추가

--- todo-react-app/src/AddTodo.js
+++ todo-react-app/src/AddTodo.js
@@ -6,6 +6,26 @@ class AddTodo extends React.Component {
     constructor(props) {
         super(props);
         this.state = { item: { title: "" } };
+        this.add = props.add;
+    }
+
+    // input text save 
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+        console.log(thisItem);
+    }
+
+    onButtonClick = () => {
+        this.add(this.state.item); //App`s add()
+        this.setState({ item: { title: "" } });
+    }
+
+    enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter') {
+            this.onButtonClick();
+        }
     }
 
     render() {
@@ -14,10 +34,21 @@ class AddTodo extends React.Component {
             <Paper style={{ margin: 16, padding: 16 }}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                        <TextField placeholder="Add Todo here" fullWidth />
+                        <TextField 
+                        placeholder="Add Todo here" 
+                        fullWidth 
+                        onChange={this.onInputChange}
+                        value={this.state.item.title}
+                        onKeyPress={this.enterKeyEventHandler}
+                        />
                     </Grid>
                     <Grid xs={1} md={1} item>
-                        <Button fullWiidth color="secondary" variant="outlined">
+                        <Button 
+                        fullWiidth 
+                        color="secondary" 
+                        variant="outlined"
+                        onClick={this.onButtonClick}
+                        >
                              +
                         </Button>
                     </Grid>
