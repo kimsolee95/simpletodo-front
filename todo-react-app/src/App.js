ca8f6499964b464d2b450ec55cf11553ed788b51
kimsolee <ahhasolee@naver.com>
2022-01-08 12:23:10 +0900

    delete function 작성

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -25,13 +25,23 @@ class App extends React.Component {
     console.log("items: ", this.state.items);
   }
 
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before update items =>", this.state.items);
+
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items =>", this.state.items);
+    });
+  }
+
   render() {
     
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
